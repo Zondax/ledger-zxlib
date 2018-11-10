02b6d4eec97cc07c4ff9bbac74a54b907af7de08
Juan Leni <lenijuan@gmail.com>
2018-11-10 20:20:35 +0100

    adding buffering support

--- include/buffering.h
+++ include/buffering.h
@@ -1,4 +1,5 @@
 /*******************************************************************************
+*   (c) 2016 Ledger
 *   (c) 2018 ZondaX GmbH
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,87 +14,64 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
-#pragma once
-#include "zxmacros.h"
 
-#ifdef LEDGER_SPECIFIC
-typedef uint16_t BUFFER_IDX_TYPE;
-#else
-typedef size_t BUFFER_IDX_TYPE;
+#ifndef CI_TEST_BUFFERING_H
+#define CI_TEST_BUFFERING_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 typedef struct {
-  uint8_t* data;
-  BUFFER_IDX_TYPE size;
-  BUFFER_IDX_TYPE pos_r;
-  BUFFER_IDX_TYPE pos_w;
+    uint8_t *data;
+    uint16_t size;
+    uint16_t pos;
+    uint8_t in_use: 1;
+    uint8_t initialized : 1;
 } buffer_state_t;
 
-__INLINE BUFFER_IDX_TYPE CLIP(BUFFER_IDX_TYPE min, BUFFER_IDX_TYPE val, BUFFER_IDX_TYPE max)
-{
-    if (val<min) return min;
-    if (val>max) return max;
-    return val;
+typedef void (*append_buffer_delegate)(buffer_state_t *buffer, uint8_t *data, int size);
+
+/// Initialize buffer
+/// \param ram_buffer
+/// \param ram_buffer_size
+/// \param ram
+/// \param flash_buffer
+/// \param flash_buffer_size
+/// \param flash
+void buffering_init(uint8_t *ram_buffer,
+                    uint16_t ram_buffer_size,
+                    append_buffer_delegate ram,
+                    uint8_t *flash_buffer,
+                    uint16_t flash_buffer_size,
+                    append_buffer_delegate flash);
+
+/// Reset buffer
+void buffering_reset();
+
+/// Append data to the buffer
+/// \param data
+/// \param length
+/// \return It returns an error message if the buffer is too small.
+char* buffering_append(uint8_t *data, int length);
+
+/// buffering_get_ram_buffer
+/// \return
+buffer_state_t *buffering_get_ram_buffer();
+
+/// buffering_get_flash_buffer
+/// \return
+buffer_state_t *buffering_get_flash_buffer();
+
+/// buffering_get_buffer
+/// \return
+buffer_state_t *buffering_get_buffer();
+
+#ifdef __cplusplus
 }
+#endif
 
-__INLINE bool eof(const buffer_state_t* buffer_state)
-{
-    return buffer_state->pos_r==buffer_state->pos_w;
-}
-
-__INLINE bool full(const buffer_state_t* buffer_state)
-{
-    return buffer_state->pos_w==buffer_state->size;
-}
-
-__INLINE BUFFER_IDX_TYPE space_left(const buffer_state_t* buffer_state)
-{
-    return (buffer_state->pos_w>buffer_state->size) ?
-           (BUFFER_IDX_TYPE) 0 :
-           buffer_state->size-buffer_state->pos_w;
-}
-
-__INLINE void resetBuffet(buffer_state_t* buffer_state)
-{
-    buffer_state->pos_r = 0;
-    buffer_state->pos_w = 0;
-}
-
-__INLINE void set_pos_r(buffer_state_t* buffer_state, BUFFER_IDX_TYPE new_pos_r)
-{
-    buffer_state->pos_r = CLIP(0, new_pos_r, buffer_state->pos_w);
-}
-
-__INLINE void set_pos_w(buffer_state_t* buffer_state, BUFFER_IDX_TYPE new_pos_w)
-{
-    buffer_state->pos_w = CLIP(0, new_pos_w, buffer_state->size);
-    buffer_state->pos_r = CLIP(0, buffer_state->pos_r, buffer_state->pos_w);
-}
-
-__INLINE void initBuffer(buffer_state_t* buffer_state, uint8_t* buffer, BUFFER_IDX_TYPE buffer_size)
-{
-    buffer_state->data = buffer;
-    buffer_state->size = buffer_size;
-    resetBuffet(buffer_state);
-}
-
-__INLINE BUFFER_IDX_TYPE getChunk(buffer_state_t* buffer_state, uint8_t* chunk, BUFFER_IDX_TYPE max_chunk_size)
-{
-    const BUFFER_IDX_TYPE src_n = CLIP(0, buffer_state->pos_w-buffer_state->pos_r, max_chunk_size);
-    const uint8_t* src_p = buffer_state->data+buffer_state->pos_r;
-
-    memcpy((void*) chunk, src_p, src_n);
-    buffer_state->pos_r += src_n;
-
-    return src_n;
-}
-
-__INLINE BUFFER_IDX_TYPE putChunk(buffer_state_t* buffer_state, const uint8_t* chunk, BUFFER_IDX_TYPE chunk_size)
-{
-    const BUFFER_IDX_TYPE dst_n = CLIP(0, chunk_size, buffer_state->size-buffer_state->pos_w);
-    uint8_t* const dst_p = buffer_state->data+buffer_state->pos_w;
-    memcpy(dst_p, chunk, dst_n);
-    buffer_state->pos_w += dst_n;
-
-    return dst_n;
-}
+#endif //CI_TEST_BUFFERING_H
