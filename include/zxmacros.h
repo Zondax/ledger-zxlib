60ff569133ef94ad905b40bc996ce52c0cfd64af
Juan Leni <lenijuan@gmail.com>
2019-12-30 15:09:53 +0100

    fix bip44 path formatting

--- include/zxmacros.h
+++ include/zxmacros.h
@@ -142,10 +142,10 @@ NUM_TO_STR(uint64)
 __Z_INLINE void bip44_to_str(char *s, uint32_t max, const uint32_t path[5]) {
     snprintf(s, max, "%d%s%d%s%d%s%d%s%d%s",
              path[0] & 0x7FFFFFFFu, (path[0] & 0x80000000u) != 0 ? "'/" : "/",
-             path[1] & 0x7FFFFFFFu, (path[0] & 0x80000000u) != 0 ? "'/" : "/",
-             path[2] & 0x7FFFFFFFu, (path[0] & 0x80000000u) != 0 ? "'/" : "/",
-             path[3] & 0x7FFFFFFFu, (path[0] & 0x80000000u) != 0 ? "'/" : "/",
-             path[4] & 0x7FFFFFFFu, (path[0] & 0x80000000u) != 0 ? "'" : "");
+             path[1] & 0x7FFFFFFFu, (path[1] & 0x80000000u) != 0 ? "'/" : "/",
+             path[2] & 0x7FFFFFFFu, (path[2] & 0x80000000u) != 0 ? "'/" : "/",
+             path[3] & 0x7FFFFFFFu, (path[3] & 0x80000000u) != 0 ? "'/" : "/",
+             path[4] & 0x7FFFFFFFu, (path[4] & 0x80000000u) != 0 ? "'" : "");
 }
 
 __Z_INLINE int8_t str_to_int8(const char *start, const char *end, char *error) {
