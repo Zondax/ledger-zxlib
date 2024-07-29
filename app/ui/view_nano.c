4b60244c489e6d4e0e48a7f38272dadd24415ef6
Andrés Benso <andresbenso@gmail.com>
2024-07-29 09:42:11 -0300

    Add generic operation  (#142)
    * add generic operation
    
    * bump version

--- app/ui/view_nano.c
+++ app/ui/view_nano.c
@@ -222,6 +222,7 @@ zxerr_t h_review_update_data() {
                     intro_value = PIC(review_addrvalue);
                     break;
 
+                case REVIEW_GENERIC:
                 case REVIEW_TXN:
                 default:
                     intro_value = PIC(review_txvalue);
