4e8185c3359348dc4b41b1cc628d0b4bcd237094
Carlos Medeiros <carlos.medeiros@zondax.ch>
2024-12-04 14:13:03 +0000

    bump container & version

--- include/zxversion.h
+++ include/zxversion.h
@@ -15,6 +15,6 @@
  ********************************************************************************/
 #pragma once
 
-#define ZXLIB_MAJOR 29
-#define ZXLIB_MINOR 6
+#define ZXLIB_MAJOR 30
+#define ZXLIB_MINOR 0
 #define ZXLIB_PATCH 0
