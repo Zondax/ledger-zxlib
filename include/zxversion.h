7c32ce0b33232990c87f6f263b6593f96496daa3
abenso <andresbenso@gmail.com>
2025-03-24 13:18:33 -0300

    bump major

--- include/zxversion.h
+++ include/zxversion.h
@@ -15,6 +15,6 @@
  ********************************************************************************/
 #pragma once
 
-#define ZXLIB_MAJOR 33
+#define ZXLIB_MAJOR 34
 #define ZXLIB_MINOR 0
 #define ZXLIB_PATCH 0
