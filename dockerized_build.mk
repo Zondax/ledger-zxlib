e01a96ae23bbd9acc8d20e14bf206888e0023389
0xPxt <107792863+0xPxt@users.noreply.github.com>
2025-12-16 18:22:58 +0100

    bump container (#213)

--- dockerized_build.mk
+++ dockerized_build.mk
@@ -54,7 +54,7 @@ $(info TESTS_ZEMU_DIR        : $(TESTS_ZEMU_DIR))
 $(info TESTS_JS_DIR          : $(TESTS_JS_DIR))
 $(info TESTS_JS_PACKAGE      : $(TESTS_JS_PACKAGE))
 
-DOCKER_IMAGE_ZONDAX=zondax/ledger-app-builder:ledger-2cd3a1314814ad2593e54a3ebe635231d0165c72
+DOCKER_IMAGE_ZONDAX=zondax/ledger-app-builder:ledger-2e5016d2d91cfaba6c89531d5fe4ead264fc9be2
 DOCKER_IMAGE_LEDGER=ghcr.io/ledgerhq/ledger-app-builder/ledger-app-builder:latest
 
 ifdef INTERACTIVE
